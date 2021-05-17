7ac91ec8dd949faa5a4e11a3540f92dac8a06f86
AlexanderAlatorreGovea <alexander_alatorre1993@hotmail.com>
2021-05-17 17:25:21 -0600

    fixed styling

--- src/App.js
+++ src/App.js
@@ -1,63 +1,90 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import WOW from "wowjs";
 
-import Header from './Header';
-import TopImage from './TopImage';
-import OurStory from './OurStory';
-import SpecialMenu from './SpecialMenu';
-import RandomQuote from './RandomQuote';
-import ContactUs from './ContactUs';
-import Reviews from './Reviews';
-import Footer from './Footer'
-import globalState from './staticData/globalState';
-import './sass/main.scss';
-import './sass/_reset.scss';
-import './sass/_variables.scss';
+import Header from "./Header";
+import TopImage from "./TopImage";
+import OurStory from "./OurStory";
+import SpecialMenu from "./SpecialMenu";
+import RandomQuote from "./RandomQuote";
+import ContactUs from "./ContactUs";
+import Reviews from "./Reviews";
+import Footer from "./Footer";
+import globalState from "./staticData/globalState";
+import "./sass/main.scss";
+import "./sass/_reset.scss";
+import "./sass/_variables.scss";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       globalState,
-      count: 0, 
+      count: 0,
       review: [
         {
-          author: 'Joe Doe',
-          company: 'The Food Network',
-          highlight: 'Best Restaurant in the L.A. Area',
-          authorInfo: 'Winner Of The Cheff Masters',
-          review: 'Lorem ipsum dolor amet truffaut lo-fi cold-pressed, shoreditch la croix snackwave aesthetic. Godard single-origin coffee locavore, mumblecore pabst photo booth deep v 3 wolf moon meh bitters biodiesel tote bag venmo succulents affogato.'
+          author: "Joe Doe",
+          company: "The Food Network",
+          highlight: "Best Restaurant in the L.A. Area",
+          authorInfo: "Winner Of The Cheff Masters",
+          review:
+            "Lorem ipsum dolor amet truffaut lo-fi cold-pressed, shoreditch la croix snackwave aesthetic. Godard single-origin coffee locavore, mumblecore pabst photo booth deep v 3 wolf moon meh bitters biodiesel tote bag venmo succulents affogato.",
         },
         {
-          author: 'Jhnny Doe',
-          company: 'HBO',
-          authorInfo: 'Owner of the Greates Restaurant',
-          review: 'Polaroid adaptogen enamel pin iceland dreamcatcher subway tile mixtape, pop-up trust fund pok pok street art fanny pack flannel pinterest. Banjo fixie 8-bit XOXO 3 wolf moon bitters af +1 migas craft beer. Lo-fi pour-over chicharrones, raw denim raclette blog stumptown readymade.'
+          author: "Jhnny Doe",
+          company: "HBO",
+          authorInfo: "Owner of the Greates Restaurant",
+          review:
+            "Polaroid adaptogen enamel pin iceland dreamcatcher subway tile mixtape, pop-up trust fund pok pok street art fanny pack flannel pinterest. Banjo fixie 8-bit XOXO 3 wolf moon bitters af +1 migas craft beer. Lo-fi pour-over chicharrones, raw denim raclette blog stumptown readymade.",
         },
         {
-          author: 'Harold Doe',
-          company: 'Times',
-          authorInfo: 'Winner Of Master Cheff',
-          review: 'Gentrify fam try-hard, thundercats tacos portland schlitz offal listicle PBR&B twee kombucha bitters. Cloud bread sustainable slow-carb brooklyn woke chartreuse. La croix blue bottle glossier sustainable. 3 wolf moon heirloom schlitz street art messenger bag.'
-        }
-    ]
+          author: "Harold Doe",
+          company: "Times",
+          authorInfo: "Winner Of Master Cheff",
+          review:
+            "Gentrify fam try-hard, thundercats tacos portland schlitz offal listicle PBR&B twee kombucha bitters. Cloud bread sustainable slow-carb brooklyn woke chartreuse. La croix blue bottle glossier sustainable. 3 wolf moon heirloom schlitz street art messenger bag.",
+        },
+      ],
+      indexOfComment: 0,
     };
   }
-  
+
+  changeQuote = () => {
+  if(this.state.indexOfComment !== 2) {
+      this.setState({
+        indexOfComment: this.state.indexOfComment + 1
+      });
+    }
+  };
+
+  reverseQuote = () => {
+    if(this.state.indexOfComment !== 0) {
+      this.setState({
+        indexOfComment: this.state.indexOfComment - 1
+      });
+    }
+  };
+
   componentDidMount() {
     const wow = new WOW.WOW();
     wow.init();
   }
-  
+
+
+
   render() {
     return (
       <div>
-        <Header  />
+        <Header />
         <TopImage />
         <OurStory />
         <SpecialMenu globalState={globalState} />
         <RandomQuote />
-        <Reviews RandomQuote={this.state}/>
+        <Reviews
+        changeQuote={this.changeQuote}
+        reverseQuote={this.reverseQuote}
+          RandomQuote={this.state.review}
+          indexOfComment={this.state.indexOfComment}
+        />
         <ContactUs />
         <Footer />
       </div>
--- src/Reviews.js
+++ src/Reviews.js
@@ -1,36 +1,70 @@
-import React from 'react';
-import './sass/Reviews.scss';
+import React from "react";
+import "./sass/Reviews.scss";
 
 class Reviews extends React.Component {
-
-    render() {
+  render() {
     return (
-        <div className="Reviews" id="Reviews">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 ">
-                        <img className="wow slideInLeft" src='https://s-i.huffpost.com/gen/1282658/images/o-FEMALE-CHEF-facebook.jpg'/>
-                    </div>
-                    <div className="col-md-4 wow slideInRight">
-                        <h5 className="comp-title">Reviews</h5>
-                        <h2>The Food Network</h2>
-                        <h4>
-                        "Best Restaurant in the L.A. Area"
-                        </h4>
-                        <p>
-                        Lorem ipsum dolor amet truffaut lo-fi cold-pressed, shoreditch la croix snackwave aesthetic. Godard single-origin coffee locavore, mumblecore pabst photo booth deep v 3 wolf moon meh bitters biodiesel tote bag venmo succulents affogato. 
-                        </p>
-                        <div className="author"><strong>Joe Doe</strong> <span></span> - <em>winner of the cheff masters</em> </div>
-                        <div className="arrows">
-                            <i class="arrow left icon"></i>
-                            <i class="arrow right icon ready"></i>
-                        </div>
-                    </div>
-                </div>
+      <div className="Reviews" id="Reviews">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 ">
+              <img
+                className="wow slideInLeft"
+                src="https://s-i.huffpost.com/gen/1282658/images/o-FEMALE-CHEF-facebook.jpg"
+              />
             </div>
+            <div className="col-md-4 wow slideInRight">
+              <h5 className="comp-title">Reviews</h5>
+
+              <div>
+                <h2>
+                  {this.props.RandomQuote[this.props.indexOfComment].company}
+                </h2>
+                <h4>
+                  {this.props.RandomQuote[this.props.indexOfComment].highlight
+                    ? this.props.RandomQuote[this.props.indexOfComment]
+                        .highlight
+                    : ""}
+                </h4>
+                <p>
+                  {this.props.RandomQuote[this.props.indexOfComment].review}
+                </p>
+                <div className="author">
+                  <strong>
+                    {this.props.RandomQuote[this.props.indexOfComment].author}{" "}
+                  </strong>{" "}
+                  <span></span> -{" "}
+                  <em>
+                    {
+                      this.props.RandomQuote[this.props.indexOfComment]
+                        .authorInfo
+                    }
+                  </em>{" "}
+                </div>
+              </div>
+
+              <div className="arrows">
+                <i
+                  onClick={this.props.reverseQuote}
+                  style={{
+                    color: this.props.indexOfComment < 2 ? "#ADADAD" : "black",
+                  }}
+                  class="arrow left icon"
+                ></i>
+                <i
+                  onClick={this.props.changeQuote}
+                  style={{
+                    color: this.props.indexOfComment > 0 ? "#ADADAD" : "black",
+                  }}
+                  class="arrow right icon ready"
+                ></i>
+              </div>
+            </div>
+          </div>
         </div>
-        );
-    }
-};
+      </div>
+    );
+  }
+}
 
 export default Reviews;
